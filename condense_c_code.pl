8a5a7974f82633cfa497ee159a2af234039e9cea
Pierre Migeon <pierre@616.local>
2019-11-18 20:09:56 -0800

    pretty nice

--- condense_c_code.pl
+++ condense_c_code.pl
@@ -1,17 +1,14 @@
 #!/usr/bin/perl -w
 use strict;
 use Term::ANSIColor;
+use POSIX;
 
 #Description: This is a script that checks your code for any unused functions. A summary table is printed if the -p flag is used. The functions that are just listed 1x are not used in the program and should probably be removed, ultimately. The script begins by catting all of the C src files into one file, then running through and removing the unused functions. This happens cyclically, until all the unused portions of the code have been removed. This is necessary because the first unused function calls other functions that become unused once the first calling function is removed. After all unused code is removed, the code is distrubted to files containing only 5 functions each. Note that this script also removes spaces where there are more than one space in a row. Script should be run in the directory that contains the code being operated on.
 
 #usage: perl id_unused_functions.pl
 
-`touch temp_combo.c && rm temp_combo.c`;
-`ls -r *.c | xargs cat >> temp_combo.c`;
-
-my $total_function_calls_1 = 0;
-my $total_function_calls_2 = -1;
 my $i = 0;
+my $continue = 1;
 
 my $print_flag = 0;
 my $split_flag = 0;
@@ -21,7 +18,7 @@ if (defined($ARGV[0]))
 	if ($ARGV[0] eq "-s") {$split_flag = 1;}
 	if ($ARGV[0] eq "-h" || $ARGV[0] eq "--help")
 	{
-		print color("green"), "\nDescription: \n\t\tScript to clean up C project and remove unused functions\nUsage:\n\t\tperl id_unused_functions.pl [-h] [-p]\n\t\tUse -p to print summary tables and -h or --help to print this message\n\n", color("reset");
+		print color("green"), "\nDescription: \n\t\tScript to clean up C project and remove unused functions\nUsage:\n\t\tperl condense_c_code.pl [-h] [-p] [-s]\n\t\tUse -p to print summary tables, -s to split sum code into sub-files and -h or --help to print this message\n\n", color("reset");
 	exit;
 	}
 }
@@ -31,16 +28,25 @@ if (defined($ARGV[1]))
 	if ($ARGV[1] eq "-s") {$split_flag = 1;}
 }
 
-while ($total_function_calls_1 != $total_function_calls_2)
+`touch temp_combo.c && rm temp_combo.c`;
+`ls -r *.c | xargs cat >> temp_combo.c`;
+
+my @num_functions;
+my $header;
+while ($continue)
 {
+	$continue = 0;
 	my %functions;
-	set_function_calls_vars($total_function_calls_1, $total_function_calls_2, $i);
 	open(IN, '<', "temp_combo.c");
 	while(<IN>)
 	{
 		if ($_ =~ m/(^char|^int|^void)[\t\s]+\*?\*?(.*)\(.*\)/)
 		{
 			$functions{$2} = 0;
+		}
+		if ($_ =~ m/(\#include.*\.h.*\n)/)
+		{
+			$header = $1;
 		}
 	}
 	close IN;
@@ -52,7 +58,7 @@ while ($total_function_calls_1 != $total_function_calls_2)
 			my $line = $_;
 			foreach(keys %functions)
 			{
-				if ($line =~ m/$_/)
+				if ($line =~ m/[^A-Za-z0-9_]$_\(/)
 				{
 					$functions{$_}++;
 				}
@@ -64,19 +70,15 @@ while ($total_function_calls_1 != $total_function_calls_2)
 	#Print the summary table:
         if ($print_flag)
         {
-		if  ($total_function_calls_2 == -1) { printf("\n\n\n"); }
+		if  ($i == 0) { printf("\n\n\n"); }
 		printf("\t%-25s\tNumber of Calls\n", "Function");
 		printf("\t------------------------------------------------\n");
 	}
 	foreach(keys(%functions))
 	{
-		if ($i % 2 == 0)
+		if (($functions{$_} == 1) && $_ !~ m/^main$/ )
 		{
-			$total_function_calls_1 += $functions{$_};
-		}
-		else
-		{
-			$total_function_calls_2 += $functions{$_};
+			$continue = 1;
 		}
 		if ($print_flag)
 		{
@@ -127,25 +129,48 @@ while ($total_function_calls_1 != $total_function_calls_2)
 	open (my $fh, '>', "./temp.c");
 	print $fh $outfile;
 	`mv  temp.c temp_combo.c`;
+	$num_functions[$i] = keys %functions;
 	$i++;
 }
 
-#under construction...
+if ($split_flag == 1)
+{
+	printf ("Removed %i functions... The remaining %i functions were split up into files containing 5 functions each\n", $num_functions[0] - $num_functions[$i - 1], $num_functions[$i - 1]);
+}
+else
+{
+	printf ("Use the -s flag to remove %i functions... %i functions will remain in the program\n", $num_functions[0] - $num_functions[$i - 1], $num_functions[$i - 1]);
+}
+
 #Now to split it up into files with no more than 5 functions each:
-#if ($split_flag == 1)
-#{
-#	$i = 0;
-#	my $j = 0;
-#	open(IN, '<', "temp_combo.c");
-#        while(<IN>){
-#		if ($_ 
-#
-#}
+if ($split_flag == 1)
+{
+	`if [ ! -d "./tmp/" ]; then mkdir ./tmp/; fi`;
+	my $outfile2 = $header;
+	$i = ceil($num_functions[$i - 1] / 5);
+	my $j = 0;
+	open(IN, '<', "temp_combo.c");
+        while(<IN>){
+		if ($_ =~ m/^}/) { $j++; }
+		unless ($_ =~ m/$header/) { $outfile2 .= $_; }
+		if ($j == 5 || eof)
+		{
+			open (my $fh, '>', "./tmp/push_swap_$i.c");
+			print $fh $outfile2;
+			$outfile2 = $header;
+			$j = 0;
+			$i--;
+		}
+	}
+	close IN;
+}
+`rm temp_combo.c`;
 
 #################
 ## Subroutines ##
 #################
 #This sets the function that counts the function call occurences to zero
+#no longer used, kept for posterity...
 sub set_function_calls_vars
 {
 	my $i;
